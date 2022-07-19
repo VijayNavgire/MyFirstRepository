049dc77419d140c0fb9a3fd04605e65119e106ce
VIJAY NAVGIRE <VIJAY NAVGIRE@DESKTOP-5116VPI>
2022-07-19 18:51:07 +0530

    [Vijay] : Its My First Repositiory on Git

---
+++ src/GitPractice/GIt_1.java
@@ -0,0 +1,12 @@
+package GitPractice;
+
+public class GIt_1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi");
+		System.out.println("Hello");
+		System.out.println("How are you");
+	}
+
+}
